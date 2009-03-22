aa87ef69a18d8ac807d92f057178f6b0be9c81c7
fivethreeo <none@none>
2009-03-22 22:32:04 +0100

    fixed a silly mistake and a bug with moving a root node as first-sibling

--- easytree/managers.py
+++ easytree/managers.py
@@ -363,7 +363,7 @@ class EasyTreeManager(models.Manager):
         cursor.execute(sql, params)
         
         logging.debug('%s %s' %  (dest_tree, fromobj.tree_id))
-        if dest_tree != fromobj.tree_id and self.is_root(target):
+        if dest_tree != fromobj.tree_id and self.is_root(target) and pos != 'first-sibling':
             sql, params = self._move_tree_left(fromobj.tree_id)
             cursor.execute(sql, params)
             
