1d72b31dce5ca470d249989f0eb20d06bc1afa21
fivethreeo <none@none>
2009-08-12 23:31:05 +0200

    added setuptools compatiblity

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1-dev'
+
+setup(name='django-easytree',
+      version=version,
+      description="Another attempt at modified preordered tree traversal for Django.",
+      long_description="", 
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='python django tree mptt',
+      author='Oyvind Saltvik',
+      author_email='oyvind.saltvik@gmail.com',
+      url='http://bitbucket.org/fivethreeo/django-easytree',
+      license='',
+      packages=['easytree',],
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
