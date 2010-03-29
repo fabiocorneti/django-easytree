a0e674e9a160b377241aa0488a690eb6c60fdf42
Fabio Corneti <info@corneti.com>
2010-03-29 16:04:16 +0200

    App is now PostgreSQL specific

--- easytree/tests/settings.py
+++ easytree/tests/settings.py
@@ -1,7 +1,27 @@
 import os
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'test.db')
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'easytree'
+DATABASE_USER = 'easytree'
+DATABASE_PASSWORD = 'easytree'
+
+INSTALLED_APPS = (
+    'silverbullet.geo',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'pound.accountmanager',
+    'pound.accountmanager.tests',
+    'burraco',
+)
+SITE_ID = 1
+
+ROOT_URLCONF = ''
+
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
